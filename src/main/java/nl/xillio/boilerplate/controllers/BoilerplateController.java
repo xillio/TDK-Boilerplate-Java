55831e43e6af3b9190458cd731dec28228484056
Sergei Filitov <sergei.filitov@xillio.com>
2022-04-28 10:50:27 +0200

    Add basic endpoints

--- src/main/java/nl/xillio/boilerplate/controllers/BoilerplateController.java
+++ src/main/java/nl/xillio/boilerplate/controllers/BoilerplateController.java
@@ -1,14 +1,40 @@
 package nl.xillio.boilerplate.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class BoilerplateController {
 
-    @GetMapping("/")
-    public String index()
+    @PostMapping("/deliver-content-metadata/{contentId}")
+    public Map<String, Object> deliverContentMetadata(
+            @PathVariable("contentId") UUID contentId,
+            @RequestBody Map<String, Object> metadata)
     {
-        return "Greetings from Spring Boot!";
+        return null;
+    }
+
+    @GetMapping("/download-content/{contentId}")
+    public Map<String, Object> downloadBinaryContent(@PathVariable("contentId") UUID contentId)
+    {
+        return null;
+    }
+
+    @PostMapping("/upload-content/{contentId}")
+    public Map<String, Object> uploadBinaryContent(
+            @PathVariable("contentId") UUID contentId,
+            @RequestBody Map<String, Object> metadata)
+    {
+        return null;
+    }
+
+    @PostMapping("upload-translation/{translationId}")
+    public Map<String, Object> uploadTranslation(
+            @PathVariable("translationId") UUID translationId,
+            @RequestBody Map<String, Object> metadata)
+    {
+        return null;
     }
 }
