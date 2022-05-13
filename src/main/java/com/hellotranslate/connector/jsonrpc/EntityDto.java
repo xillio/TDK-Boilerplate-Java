118d5c9c5b0b0dab87aa0a4c0856dafc4f696958
Sergei Filitov <sergei.filitov@xillio.com>
2022-05-13 14:51:06 +0200

    Adjust EntityDto

--- src/main/java/com/hellotranslate/connector/jsonrpc/EntityDto.java
+++ src/main/java/com/hellotranslate/connector/jsonrpc/EntityDto.java
@@ -2,10 +2,10 @@ package com.hellotranslate.connector.jsonrpc;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.hellotranslate.connector.decorators.Modified;
 import com.hellotranslate.connector.decorators.Original;
+import com.hellotranslate.connector.jsonrpc.request.XDIP;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record EntityDto(String id, String xdip, String kind, Original original, Modified modified){
+public record EntityDto(String id, XDIP xdip, String kind, Original original, Original modified) {
 }
