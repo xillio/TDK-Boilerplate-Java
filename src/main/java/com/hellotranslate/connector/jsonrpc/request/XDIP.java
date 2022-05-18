7122a99875e11927d6c0eaf77f9cb4a2f76ba39f
Sergei Filitov <sergei.filitov@xillio.com>
2022-05-18 07:29:54 +0200

    Refactor getParamsExcept() from XDIP

--- src/main/java/com/hellotranslate/connector/jsonrpc/request/XDIP.java
+++ src/main/java/com/hellotranslate/connector/jsonrpc/request/XDIP.java
@@ -239,11 +239,11 @@ public class XDIP {
         }
     }
 
-    private List<NameValuePair> getParamsExcept(String parameterName) {
+    private List<? extends NameValuePair> getParamsExcept(String parameterName) {
         return encodedQueryParameters.entrySet()
                 .stream()
                 .filter(set -> !parameterName.equals(set.getKey()))
                 .map(set -> new BasicNameValuePair(decode(set.getKey()), decode(set.getValue())))
-                .collect(Collectors.toList());
+                .toList();
     }
 }
